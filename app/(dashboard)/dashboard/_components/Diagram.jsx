eb5655e60329d7e6a0866865b7be7bf7241b6583
Ionita Alexandru <alexvladut03@yahoo.com>
2024-09-26 00:38:37 +0300

    Refactor diagram styling and add click functionality to each node in the diagram

--- app/(dashboard)/dashboard/_components/Diagram.jsx
+++ app/(dashboard)/dashboard/_components/Diagram.jsx
@@ -5,33 +5,47 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { useRouter } from "next/navigation";
 import mermaid from "mermaid";
 import { useEffect } from "react";
+
 // Definim user-ul curent
 const currentUser = "alex"; // Poți schimba rolul în funcție de utilizator
 
 // Definim permisiunile fiecărui utilizator
 const userPermissions = {
-  alex: { accessibleNodes: [1, 2, 22, 4, 5, 6, 7, 8] },
+  alex: { accessibleNodes: [108, 31, 18, 21, 6, 2, 4, 3] },
   // Poți adăuga alți utilizatori aici
 };
 
 // Funcție care verifică dacă utilizatorul are acces la un anumit nod
 const hasAccess = (id) => {
   const userPermission = userPermissions[currentUser];
-  return userPermission.accessibleNodes.includes(id);
+  return userPermission?.accessibleNodes.includes(id);
 };
 
-// Funcție care stilizează nodurile accesibile și inaccesibile în diagramă
+// Funcție care stilizează nodurile accesibile și inaccesibile în diagramă și adaugă apeluri click la final
 const getStyledDiagram = (diagram) => {
   let styledDiagram = diagram.replace(/(\d+)\[.*?\]/g, (match, id) => {
     const elementId = parseInt(id, 10);
     if (hasAccess(elementId)) {
-      // Stilizăm nodurile accesibile cu o clasă specială
+      // Stilizăm nodurile accesibile
       return `${match}:::accessible`;
     } else {
-      // Stilizăm nodurile inaccesibile cu o clasă specială
+      // Stilizăm nodurile inaccesibile
       return `${match}:::inaccessible`;
     }
   });
+
+  // Colectăm apelurile de click doar pentru nodurile accesibile
+  const clickEvents = diagram
+    .match(/(\d+)\[.*?\]/g)
+    .map((match) => {
+      const elementId = parseInt(match.match(/\d+/)[0], 10);
+      if (hasAccess(elementId)) {
+        return `click ${elementId} callFunction`;
+      }
+      return null;
+    })
+    .filter(Boolean)
+    .join("\n");
 
   // Definim clasele pentru stilizarea nodurilor accesibile și inaccesibile
   styledDiagram += `
@@ -39,8 +53,10 @@ const getStyledDiagram = (diagram) => {
     classDef inaccessible fill:red,fill-opacity:0.7,color:black,stroke:black,stroke-width:2px,rx:5px,opacity:0.7;
   `;
 
-  return styledDiagram;
+  // Adăugăm click-urile la finalul diagramei
+  return styledDiagram + "\n" + clickEvents;
 };
+
 export const Diagram = () => {
   const router = useRouter();
 
@@ -57,7 +73,10 @@ export const Diagram = () => {
     };
     mermaid.initialize(config); // Inițializăm mermaid cu configurarea dorită
   }, [router]);
+
+  // Aplicăm stilizarea diagrama
   const styledDiagram = getStyledDiagram(diagram);
+
   return (
     <TransformWrapper limitToBounds={false} centerOnInit={true}>
       <TransformComponent>
