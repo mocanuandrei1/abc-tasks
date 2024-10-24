931dfc296509928eb5dccc8274bd3cadf643db51
mocanu <mocanuandrei61@gmail.com>
2024-10-24 16:19:35 +0300

    Refactor page.js to add an iframe for embedding Miro board

---
+++ app/test/page.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <iframe
+        width="768"
+        height="432"
+        src="https://miro.com/app/live-embed/uXjVKm-q020=/?embedMode=view_only_without_ui&autoplay=true&moveToViewport=-23165,-5837,13803,7546"
+        frameBorder="0"
+        scrolling="no"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default page;
